ab47554a0a86b020938194ad60819242abde67d9
Cristian Morataya <cris.morataya@gmail.com>
2016-07-24 23:40:24 -0600

    Ejercicio #2

--- src/com/ccmorataya/Hello.java
+++ src/com/ccmorataya/Hello.java
@@ -13,8 +13,25 @@ public class Hello {
         numOne = Integer.parseInt(input.nextLine());
         numTwo = Integer.parseInt(input.nextLine());
         numThree = Integer.parseInt(input.nextLine());
-        System.out.println(sumaTres(numOne, numTwo, numThree));
+        System.out.println(mayorTres(numOne, numTwo, numThree));
     }
+
+    public static int mayorTres(int numOne, int numTwo, int numThree){
+        int[] numeros = {numOne, numTwo, numThree};
+        int mayor = 0;
+
+        if (numeros[0] > numeros[1] && numeros[0] > numeros[2]){
+            mayor = numeros[0];
+        }
+        else if (numeros[1] > numeros[0] && numeros[1] > numeros[2]){
+            mayor = numeros[1];
+        }
+        else if (numeros[2] > numeros[0] && numeros[2] > numeros[1]){
+            mayor = numeros[2];
+        }
+        return mayor;
+    }
+
     public static int sumaTres(int numOne, int numTwo, int numThree){
         return numOne+numTwo+numThree;
     }
